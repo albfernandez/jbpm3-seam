3f72c399653fd4b8e8dcb86d98276a48213d154b
Alberto Fernandez <infjaf@gmail.com>
2022-10-24 10:55:01 +0200

    Fix build with hsqldb 2.7.1

--- core/src/test/java/org/jbpm/DbServer.java
+++ core/src/test/java/org/jbpm/DbServer.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
 import org.hsqldb.Server;
-import org.hsqldb.util.DatabaseManager;
+//import org.hsqldb.util.DatabaseManager;
 
 /**
  * use this in combination with the HQL editor of the hibernate plugin.
@@ -14,17 +14,17 @@ import org.hsqldb.util.DatabaseManager;
 public class DbServer {
 
   public static void main(String[] args) {
-    Configuration configuration = new Configuration();
-    configuration.configure();
-    new SchemaExport().create(EnumSet.of(TargetType.SCRIPT, TargetType.DATABASE), null);
-
-    Server server = new Server();
-    server.setSilent(false);
-    server.setDatabaseName(0, "jbpm");
-    server.setDatabasePath(0, "mem:jbpm");
-    server.setPort(9001);
-    server.start();
-
-    DatabaseManager.main(new String[]{"-url", "jdbc:hsqldb:hsql://localhost:9001/jbpm"});
+//    Configuration configuration = new Configuration();
+//    configuration.configure();
+//    new SchemaExport().create(EnumSet.of(TargetType.SCRIPT, TargetType.DATABASE), null);
+//
+//    Server server = new Server();
+//    server.setSilent(false);
+//    server.setDatabaseName(0, "jbpm");
+//    server.setDatabasePath(0, "mem:jbpm");
+//    server.setPort(9001);
+//    server.start();
+//
+//    DatabaseManager.main(new String[]{"-url", "jdbc:hsqldb:hsql://localhost:9001/jbpm"});
   }
 }
